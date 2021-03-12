54c3d2d4dd4964b6bdbba3957a0125c3f5a0045a
Bryce Vandegrift <brycevandegrift@airmail.cc>
2021-03-12 17:11:23 -0500

    Added things

--- config.h
+++ config.h
@@ -57,12 +57,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, "-c", "-l", "30", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *bravecmd[] = { "brave", NULL };
 static const char *gamecmd[]  = { "lutris", NULL };
 static const char *textcmd[]  = { "st", "vim", NULL };
 static const char *mailcmd[]  = { "st", "neomutt", NULL };
+static const char *irccmd[]   = { "st", "weechat", NULL };
+static const char *emojicmd[] = { "./scripts/unicode", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -72,6 +74,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_g,	   spawn,	   {.v = gamecmd } },
 	{ MODKEY,			XK_s,	   spawn,	   {.v = textcmd } },
 	{ MODKEY,			XK_e,	   spawn,	   {.v = mailcmd } },
+	{ MODKEY,			XK_u,	   spawn,	   {.v = emojicmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
